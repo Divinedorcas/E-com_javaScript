87e76f5eba18b410c3bcf2899f05041feb39403e
Divinedorcas <Oguchedorcas1020@gmail.com>
2025-01-13 19:14:53 -0800

    changing

---
+++ calendar/calendar.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+
+let date =new Date();
+console.log(date);
+const con = document.querySelector(".container");
+console.log(con);
--- hhhh.js
+++ hhhh.js
@@ -16,3 +16,22 @@ else{
 
     
  }
+
+ //Develop a program to calculate the shipping cost based on 
+  //  the destination zone and the weight of the package (you will be provided)
+  //  Calcualte the shipping cost according to these conditions:
+  //  If the destination is  'Abuja', the shiping cost is $5 per kilogram
+  //  If the destination is 'Kaduna', the shiping cost is $7 per kilogram
+  //  If the destination is 'Lagos', the shiping cost is $10 per kilogram
+  //  If the destination is not 'Abuja', 'Kaduna', or 'Lagos', 
+  //  display an error message "We do not cover this destination
+
+ function calculateShippingCost(city, shipingPrice){
+if(city==="abuja"){
+
+}elseif(city==="kaduna"){}
+elseif(city==="lagos"){}
+else{
+    print();
+}
+ }
